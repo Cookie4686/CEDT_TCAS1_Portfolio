3fb79c2723637c59ddfcf5c3557212063e9cc8e9
Cookie4686 <117904237+Cookie4686@users.noreply.github.com>
2023-11-11 15:18:18 +0700

    You got this right?
    Preparation for publishing (Still Require Code optimization and English Transalation)

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -2,10 +2,56 @@ const topNav = document.getElementById("topNav");
 function openNav() {
   topNav.classList.toggle("navOpened");
 }
-const body = document.getElementsByTagName('body');
+
 function darkMode() {
-  body[0].classList.toggle('darkMode');
+  let theme = sessionStorage.getItem("theme");
+  if(theme=='dark'){
+    sessionStorage.setItem("theme",'light');
+    document.documentElement.setAttribute('data-theme','light');
+  }else{
+    sessionStorage.setItem("theme",'dark');
+    document.documentElement.setAttribute('data-theme','dark');
+  }
 }
+
+// const body = document.getElementsByTagName('body');
+// if(getCookie('darkMode')=='true'){
+//   body[0].classList.add('darkMode');
+// }
+// function darkMode() {
+//   let cookie = getCookie('darkMode');
+//   if(cookie=='false' || cookie==''){
+//     setCookie('darkMode',true,1);
+//     body[0].classList.add('darkMode');
+//   }else{
+//     setCookie('darkMode',false,1);
+//     body[0].classList.remove('darkMode');
+//   }
+// }
+// function setCookie(name,value,exhours) {
+//   const exTime = new Date();
+//   exTime.setTime(exTime.getTime() + (exhours*60*60*1000));
+//   let expires = "expires=" + exTime.toUTCString();
+//   document.cookie = name + "=" + value + ";" + expires + ";path=/";
+// }
+// function getCookie(cookieName) {
+//   let name = cookieName + "=";
+//   let decodedCookie = decodeURIComponent(document.cookie);
+//   let ca = decodedCookie.split(';');
+//   for(let i = 0; i < ca.length; i++) {
+//     let c = ca[i];
+//     while (c.charAt(0) == ' ') {
+//       c = c.substring(1);
+//     }
+//     if (c.indexOf(name) == 0) {
+//       return c.substring(name.length, c.length);
+//     }
+//   }
+//   return "";
+// }
+// function darkMode() {
+//   body[0].classList.toggle('darkMode');
+// }
 
 // let prevScrollpos = window.scrollY;
 // window.onscroll = function() {topNavShrink()};
